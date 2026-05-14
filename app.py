3f59be66468e5a0b3d1651dadcc7954de7c728cd
Chris MacBook <macbook@Chris-MacBook.local>
2026-05-15 00:07:24 +0200

    ajout de la cration des tables

--- app.py
+++ app.py
@@ -88,5 +88,9 @@ def sitemap():
 
 # --- Lancement de l'application ---
 if __name__ == "__main__":
+
+    with app.app_context():
+        db.create_all()
+        
     port = int(os.environ.get('PORT', 5001))
     app.run(host="0.0.0.0", port=port, debug=True)
